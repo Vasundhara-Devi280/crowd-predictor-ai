ff8255824ae07d8924d6d56938b3e4e66343181e
Vasundhara-Devi280 <vsd.vist@gmail.com>
2026-05-21 02:14:05 +0530

    Fix: match import to capital F ReportForm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import ReportForm from "./pages/Reportform";
+import ReportForm from "./pages/ReportForm";
 
 function App() {
   return (
